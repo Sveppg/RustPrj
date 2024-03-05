a4bffc8af10585ddcde5eba818a968edbcb97b4a
Sveppg <terminator12327@gmail.com>
2024-03-05 01:43:11 +0100

    changes on src file

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,12 @@ fn main() {
     let pattern = std::env::args().nth(1).expect("no pattern given!");
     let path = std::env::args().nth(2).expect("no path given");
     let col = std::env::args().nth(3).expect("no column given");
-
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::Pathbuf::from(path),
+    struct Cli{
+        pattern: String;
+        path: std::path::Pathbuf,
     };
     
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    println!("pattern: {:?}, path: {:?}, column {:?}", args.pattern, args.path, args.col );
 }
 
 //Note: Pathbuf is like a String but for file system paths that work cross-platform
