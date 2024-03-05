7043e6062bc52f1dc334807a86356e7e5461fe5c
Sveppg <terminator12327@gmail.com>
2024-03-05 01:51:21 +0100

    changes on source file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     //println!("Hello, world!");
     
@@ -9,9 +10,22 @@ fn main() {
         path: std::path::Pathbuf,
     };
     
+    guess();
+    
     println!("pattern: {:?}, path: {:?}, column {:?}", args.pattern, args.path, args.col );
 }
 
+fn guess(){
+    println!("Guess a number: ");
+    let mut guess = String::new();
+    let y = 5; 
+    let x = 10;
+    println!("x = {x}, y + 2 = {}", y+2);
+    io:stdn()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!(" Your Guess: {guess}");
+}
 //Note: Pathbuf is like a String but for file system paths that work cross-platform
 
 
